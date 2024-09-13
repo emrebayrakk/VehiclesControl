01fb5af800fd2237312f4795bd0fec5e91b56d09
Emre Bayrak <77586103+emrebayrakk@users.noreply.github.com>
2024-09-13 17:47:25 +0300

    added project blazor and mudblazor ui

---
+++ VehiclesControl.Web/ApiRequest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace VehiclesControl.Web
+{
+    public class ApiRequest(HttpClient httpClient, ProtectedLocalStorage localStorage)
+    {
+        public async Task SetAuthorizeHeader()
+        {
+            var token = (await localStorage.GetAsync<string>("token")).Value;
+            if (token != null)
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            }
+
+        }
+        public async Task<T> GetFromJsonAsync<T>(string path)
+        {
+            await SetAuthorizeHeader();
+            return await httpClient.GetFromJsonAsync<T>(path);
+        }
+        public async Task<T1> PostAsync<T1, T2>(string path, T2 postModel)
+        {
+            await SetAuthorizeHeader();
+            var res = await httpClient.PostAsJsonAsync(path, postModel);
+            if (res != null)
+            {
+                return JsonConvert.DeserializeObject<T1>(await res.Content.ReadAsStringAsync());
+            }
+            return default;
+        }
+        public async Task<T1> PutAsync<T1, T2>(string path, T2 postModel)
+        {
+            await SetAuthorizeHeader();
+            var res = await httpClient.PutAsJsonAsync(path, postModel);
+            if (res != null)
+            {
+                return JsonConvert.DeserializeObject<T1>(await res.Content.ReadAsStringAsync());
+            }
+            return default;
+        }
+        public async Task<T> DeleteAsync<T>(string path)
+        {
+            await SetAuthorizeHeader();
+            return await httpClient.DeleteFromJsonAsync<T>(path);
+        }
+    }
+}
